158c797e9713a7a0d3af91c83b9896486eb03cdc
M4uAWA <mau_jauregui@hotmail.com>
2024-08-27 19:15:22 -0600

    Update 7:15 27/08/24 Programa1.py

--- programa1/Programa1.py
+++ programa1/Programa1.py
@@ -7,6 +7,7 @@ Created on Sun Aug 25 18:58:33 2024
 
 import os
 import time
+import copy
 from process import Process
 from batch import Batch
 from queue import Queue
@@ -18,28 +19,28 @@ print("How many processes do you want to capture?\n")
 tProcesses = int(input("ANS: "))
 
 batchQueue = Queue()
-auxProcess = Process()
+
 auxBatch = Batch(bS)
 tmet = 0
 
 i = 0
 while (tProcesses != 0):
+        auxProcess = Process()
         print("\nProcess number", i + 1 ,"\n")
         auxProcess.name = input("Process name: ")
         auxProcess.x = int(input("Operand 1: "))
         auxProcess.operation = input("Operation type: ")
         auxProcess.y = int(input("Operand 2: "))
         auxProcess.pid = i
-        auxProcess.mte = int(input("Maximum estimated time: "))
+        auxProcess.met = int(input("Maximum estimated time: "))
         tmet += auxProcess.met
         
-        auxBatch.processQueue.put(auxProcess) #Process enqueue
+        #Process enqueue
         
-        if(auxBatch.processQueue.full()): #When reaching batch size, enqueue batch
-            print("Batch completed")
+        if(auxBatch.processQueue.full()):     #When reaching batch size, enqueue batch  
             batchQueue.put(auxBatch)
-            while (not auxBatch.processQueue.empty()): #Empty auxBatch processQueue (to reuse auxBatch)
-                auxBatch.processQueue.get()
+            auxBatch = Batch(bS)
+        auxBatch.processQueue.put(auxProcess)
             
         i += 1
         tProcesses -= 1 #Decrement remaining processes
@@ -50,9 +51,28 @@ if(not auxBatch.processQueue.empty()): #Case - Queuing an incomplete batch
     
 #End of first part    
 
-for i in range(tmet):
-    print("Tiempo restante: ",tmet)
-    
-    tmet -= 1
-    time.sleep(1)
-            
+while(not batchQueue.empty()):
+    showBatch = batchQueue.get()
+        #left panel
+    while (not showBatch.processQueue.empty()):
+        showCurrentbatch = Batch(bS)
+        showCurrentbatch = showBatch
+        print("\nNumber of pending batches:", batchQueue.qsize())
+        print("\n\nCurret Batch\n")
+        print("Name   MET")
+        while (not showCurrentbatch.processQueue.empty()):
+            showProcess = showCurrentbatch.processQueue.get()
+            print(showProcess.name,"    ",showProcess.met)
+        
+        print("\n\nProcessing")
+        #middle panel
+        showProcess = showBatch.processQueue.get()
+        print("\n\nProcessing")
+        print("\nID:" ,showProcess.pid)
+        print("\nOP:" ,showProcess.operation)
+        print("\nMET:" ,showProcess.met)
+        print("\nNAME:" ,showProcess.name,"\n")
+        for i in range(showProcess.met):
+            print("\rTE:",i,"\nTR:",showProcess.met-i)
+            tmet -= 1   
+            time.sleep(1)
